366ae05fb4dc56691e8b511df6cd9ea7cd9448b7
azerpas <xskylix@hotmail.fr>
2021-06-27 11:50:11 +0200

    Added mailchimp support

---
+++ src/declarations/augmented-mailchimp.d.ts
@@ -0,0 +1,28 @@
+declare module '@mailchimp/mailchimp_marketing' {
+    type Config = {
+        apiKey?: string,
+        accessToken?: string,
+        server?: string
+    }
+
+    type SetListMemberOptions = {
+        skipMergeValidation: boolean
+    }
+
+    export type SetListMemberBody = {
+        email_address: string,
+        status_if_new?: 'subscribed' | 'unsubscribed' | 'cleaned' | 'pending' | 'transactional'
+        status?: 'subscribed' | 'unsubscribed' | 'cleaned' | 'pending' | 'transactional'
+        merge_fields?: {[key: string]: any}
+        interests?: {[key: string]: any}
+    }
+
+    // eslint-disable-next-line import/no-anonymous-default-export
+    export default {
+        setConfig: (config: Config) => {},
+        lists: {
+            setListMember: (listId: string, subscriberHash: string, body: SetListMemberBody, opts?: SetListMemberOptions): Promise<any> => {},
+            addListMember: (listId: string, body: SetListMemberBody): Promise<any> => {}
+        }
+    }
+}
