22e8721d2f3209cb69d6084b9e22384417d28cdf
azerpas <xskylix@hotmail.fr>
2021-06-29 15:00:49 +0200

    Fixed graphql handler

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,28 +1,12 @@
-import { ApolloServer, gql } from 'apollo-server-micro';
+import { ApolloServer } from 'apollo-server-micro'
+import { schema } from '../../apollo/schema';
 
-const typeDefs = gql`
-    type Query {
-        users: [User!]!
-    }
-    type User {
-        name: String
-    }
-`
-
-const resolvers = {
-    Query: {
-        users(parent, args, context) {
-            return [{ name: 'Nextjs' }]
-        },
-    },
-}
-
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({ schema })
 
 export const config = {
-    api: {
-        bodyParser: false,
-    },
+  api: {
+    bodyParser: false,
+  },
 }
 
 export default apolloServer.createHandler({ path: '/api/graphql' })
