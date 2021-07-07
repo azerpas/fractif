d465e2acaf3b7750b1afeb6f138a9b4f0ef8619f
azerpas <xskylix@hotmail.fr>
2021-07-07 18:28:29 +0200

    Box Shadow to underline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,9 @@ export default function Home() {
       <CardCar/>
 
       <Flex w="100%" h="300px" bg="#16161A" mt="100px" flexDirection={{ base: 'column', sm: 'column', lg: 'row' }} >
-            <Text color="white" margin="auto" ml={{ base: '20px', sm: '20px', lg: '80px' }} mr="10px" fontWeight="extrabold" fontSize={{ base: '20px', sm: '1xl', lg: '4xl' }} w={{ base: '90%', sm: '', lg: '50%' }}>The fractional investment is the least risky investment.</Text>
+          <Text color="white" margin="auto" ml={{ base: '20px', sm: '20px', lg: '80px' }} mr="10px" fontWeight="extrabold" fontSize={{ base: '20px', sm: '1xl', lg: '4xl' }} w={{ base: '90%', sm: '', lg: '50%' }}>
+            The fractional investment is the least risky investment.
+          </Text>
           <Flex flexDirection="column" mb={{ base: '70px', sm: '70px', lg: '0' }} margin={{ base: '', sm: '', lg: 'auto' }} ml={{ base: '20px', sm: '20px', lg: '80px' }}>
             <Text color="white" fontWeight="bold" fontSize={{ base: '12px', sm: '12px', lg: '2xl' }}  w={{ base: '90%', sm: '', lg: '85%' }}>But you don’t have to take our word for it.</Text>
             <Text color="white" fontWeight="medium" fontSize={{ base: '10px', sm: '10px', lg: '15px' }}  w={{ base: '90%', sm: '', lg: '80%' }}>According to these nine Advisors in The Oracles, who 
