13f00cc7bad7866fcfe3f9097474bfaf245889ce
azerpas <xskylix@hotmail.fr>
2021-07-09 18:13:13 +0200

    Added Captcha check

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,15 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import check from '../../services/google/recaptcha';
 import { addContact } from '../../services/mailchimp';
 import { Contact } from '../../types/Contact'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === "POST"){
-        const contact: Contact = req.body;
-        const id = await addContact(contact);
-        res.status(200).json({ id });
+        const result = await check({response: req.body.response, ip: req.socket.remoteAddress});
+        const resultAsJson = await result.json();
+        if(resultAsJson.success){
+            if(resultAsJson.score > 0.3){
+                const contact: Contact = req.body;
+                const id = await addContact(contact);
+                res.status(200).json({ id });
+            }else res.status(403).send("SCORE TOO LOW");
+        }else res.status(401).send("CAPTCHA ERROR 1");
     }else{
         res.status(401).send("METHOD NOT FOUND");
     }
-    
-    
 }
