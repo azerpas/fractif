408128c7913917f8d11d087e047e6f776ada08bc
azerpas <xskylix@hotmail.fr>
2021-07-14 10:52:46 +0200

    dimensions inside Pourcentage component

--- src/pages/components/pourcentage/index.tsx
+++ src/pages/components/pourcentage/index.tsx
@@ -8,14 +8,36 @@ import {
   Center,
   Box
 } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useRef, useLayoutEffect, useState } from 'react';
 
 import GreenShadow from "../Shadow/green"
-export default function pourcentage(data) {
+
+export default function Pourcentage(data) {
+  const boxRef = useRef();
+  
+  const [dimensions, setDimensions] = useState<{width: undefined|number, height: undefined|number}>({width: undefined, height: undefined});
+  useLayoutEffect(() => {
+    if(boxRef.current){
+      setDimensions({width: boxRef.current!.offsetWidth, height: boxRef.current!.offsetHeight});
+    }
+  }, []);
+  useEffect(() => {
+    let movement_timer = null;
+    window.addEventListener('resize', ()=>{
+      clearInterval(movement_timer);
+      movement_timer = setTimeout(() => {
+        if(boxRef.current){
+          setDimensions({width: boxRef.current!.offsetWidth, height: boxRef.current!.offsetHeight});
+        }
+      }, 100);
+    });
+  }, [])
   return (
     <>
-    <Box bg={data.colorbox} boxSize={{ base: '35px', sm: '35px', lg: '50px' }} alignItems="center" >
+    <Box bg={data.colorbox} alignItems="center" ref={boxRef}>
       <Center >
-          <Text fontWeight="bold" color="blackAlpha.500" mt={{ base: '8px', sm: '8px', lg: '13px' }} fontSize={{ base: '12px', sm: '12px', lg: '' }} textAlign="center">{data.pourcentageCard}</Text>
+          <Text fontWeight="bold" color="blackAlpha.500" my="1" textAlign="center">{data.pourcentageCard}</Text>
       </Center>
     </Box>
     </>
