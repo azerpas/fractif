ab5324feec97254e4accc8f4f5596790b0edbf59
azerpas <xskylix@hotmail.fr>
2021-07-19 23:27:55 +0200

    Fixed quote escape

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home() {
           <Flex flexDirection="column" mb={{ base: '70px', sm: '70px', lg: '0' }} margin={{ base: '', sm: '', lg: 'auto' }} ml={{ base: '20px', sm: '20px', lg: '80px' }}>
             <Text color="white" fontWeight="bold" fontSize={{ base: '12px', sm: '12px', lg: '2xl' }}  w={{ base: '90%', sm: '', lg: '85%' }}>Diversify your investment and earn big.</Text>
             <Text color="white" fontWeight="medium" fontSize={{ base: '10px', sm: '10px', lg: '15px' }}  w={{ base: '90%', sm: '', lg: '80%' }}>Pre-owned popular cars prices are hitting an all time high, making some early purchases an amazing source of investment. </Text>
-            <Text color="white" as="cite" fontWeight="medium" fontSize={{ base: '10px', sm: '10px', lg: '15px' }}  w={{ base: '90%', sm: '', lg: '80%' }} my="2">"Used-car prices, which have soared in recent months, are now defying economic gravity." </Text>
+            <Text color="white" as="cite" fontWeight="medium" fontSize={{ base: '10px', sm: '10px', lg: '15px' }}  w={{ base: '90%', sm: '', lg: '80%' }} my="2">&quot;Used-car prices, which have soared in recent months, are now defying economic gravity.&quot;</Text>
             <Text color="white" fontWeight="medium" fontSize={{ base: '10px', sm: '10px', lg: '15px' }}  w={{ base: '90%', sm: '', lg: '80%' }}><WSJ/> </Text>
           </Flex>
       </Flex>
