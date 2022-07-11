e929ffd901a56cc8c0a402b7972669539fcf4b2c
Tixwell <teoc.cricelli@gmail.com>
2022-07-11 16:22:31 +0200

    [ADDED] Handle loading system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,34 @@
-import * as React from "react";
-import {AppProps} from "next/app";
+import React, { useState } from "react";
+import Router from 'next/router'
+import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import LoadingPage from "./loading";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+    // LOADING \\
+    const [loading, setLoading] = useState(false);
+    Router.events.on('routeChangeStart', () => {
+        console.log('loading...');
+        setLoading(true);
+    });
+    Router.events.on('routeChangeComplete', () => {
+        console.log('loading ended');
+        setLoading(false);
+    });
+
     return (
-        <ChakraProvider resetCSS theme={theme}>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <>
+            {
+                loading
+                ?
+                <LoadingPage />
+                :
+                <ChakraProvider resetCSS theme={theme}>
+                    <Component {...pageProps} />
+                </ChakraProvider>
+            }
+        </>
     )
 }
