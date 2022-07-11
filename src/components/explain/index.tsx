299cb96a712023ba16bd0c4bdc856dbadf6cbf85
Tixwell <teoc.cricelli@gmail.com>
2022-07-11 16:24:07 +0200

    [Update] explain component

--- src/components/explain/index.tsx
+++ src/components/explain/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import {
   Button,
@@ -9,53 +9,55 @@ import {
   Text,
   Center,
   Box,
-  Container
+  Container,
+  Stack
 } from '@chakra-ui/react';
-  
+
 export default function Explain() {
   const [getexplain, setexplain] = useState(true);
 
   return (
     <>
+    <Stack align={"center"} justify={"center"} >
+      <Stack w="80%">
       <Center mt="3rem" mb="10px">
-          <Heading fontSize="3xl" mt={3}>How It Works</Heading>
+        <Heading fontSize="3xl" mt={3}>How It Works</Heading>
       </Center>
       <Center >
         <Text fontWeight="light" fontSize={{ base: '0.9rem' }}>Buying & selling part of a car is simple, just as it should be.</Text>
       </Center>
       {
-        getexplain == true ? 
-        <>
-          <Center mt="20px" >
-            <Flex flexDirection="row">
-              <Center mr="25px">
-                <Text 
-                  textDecoration="underline" textDecorationColor="#81FF62" textDecorationThickness="2px" 
-                  fontWeight="semibold" onClick={() => setexplain(true)}>Buying
-                </Text> 
+        getexplain == true ?
+          <>
+            <Stack align={'center'} justify={'center'}>
+              <Center mt="20px">
+                <Flex flexDirection="row" >
+                  <Center mr="25px">
+                    <Text
+                      textDecoration="underline" textDecorationColor="#2F80CB" textDecorationThickness="2px"
+                      fontWeight="semibold" onClick={() => setexplain(true)}>Buying
+                    </Text>
+                  </Center>
+                  <Center ml="25px">
+                    <Text color="gray.600" fontWeight="semibold" onClick={() => setexplain(false)}>Selling</Text>
+                  </Center>
+                </Flex>
               </Center>
-              <Center ml="25px">
-                <Text color="gray.600" fontWeight="semibold" onClick={() => setexplain(false)}>Selling</Text> 
-              </Center>
-            </Flex>
-          </Center>
 
-          <Center>
-            <Container textAlign="center">
-              <Box w="90%" alignItems="center" justifyContent="center">
-                <SimpleGrid columns={{ base: 1, sm: 1, lg: 3 }} spacing={{ base: 5, lg: 10 }} mt="40px" >
-                  <Box display={{base: "block", lg: "flex"}} flexDirection="column" justifyContent="space-between">
-                    <Heading ml="15px" fontWeight="semibold" fontSize={{ base: "large" }}>1. Browse car and listings</Heading>
-                    <Text ml="15px" w={{ base: '90%' }} fontSize={{ base: '0.9rem' }} fontWeight="light" color="gray.600">Browse hundreds of 100% safe & legit listings of 
-                        the best bots, all available for instant download.</Text>
-                    <Image
-                      alt="Car listings"
-                      objectFit={'cover'}
-                      src={
-                      'https://media.discordapp.net/attachments/707421140179746946/858521545273376768/PXgdd5t.png'
-                      }/>
-                  </Box>
-                  <Box display={{base: "block", lg: "flex"}} flexDirection="column" justifyContent="space-between">
+                    <SimpleGrid columns={{ base: 1, sm: 1, lg: 3 }} spacing={{ base: 5, lg: 10 }} mt="40px" w="80%" h="" pt={{base: "0",md:"5%"}}>
+                      <Box display={{ base: "block", lg: "flex" }} flexDirection="column" justifyContent="space-between" >
+                        <Heading ml="15px" fontWeight="semibold" fontSize={{ base: "large" }}>1. Browse car and listings</Heading>
+                        <Text ml="15px" w={{ base: '90%' }} fontSize={{ base: '0.9rem' }} fontWeight="light" color="gray.600">Browse hundreds of 100% safe & legit listings of
+                          the best bots, all available for instant download.</Text>
+                        <Image
+                          alt="Car listings"
+                          objectFit={'cover'}
+                          src={
+                            'https://media.discordapp.net/attachments/707421140179746946/858521545273376768/PXgdd5t.png'
+                          } />
+                      </Box>
+
+                      <Box display={{base: "block", lg: "flex"}} flexDirection="column" justifyContent="space-between">
                     <Heading ml="15px" fontWeight="semibold" fontSize={{ base: "large" }}>2. Purchase 1 to 100% of the car</Heading>
                     <Text ml="15px" w={{ base: '90%' }} fontSize={{ base: '0.9rem' }} fontWeight="light" color="gray.600">Instantly purchase a whole car or only a fraction of it.</Text>
                     <Image
@@ -64,6 +66,7 @@ export default function Explain() {
                       alt="Filter car search form"
                       />
                   </Box>
+
                   <Box display={{base: "block", lg: "flex"}} flexDirection="column" justifyContent="space-between">
                     <Heading ml="15px" fontWeight="semibold" fontSize={{ base: "large" }}>3. Download your new bot</Heading>
                     <Text ml="15px" w={{ base: '90%' }} fontSize={{ base: '0.9rem' }} fontWeight="light" color="gray.600">No waiting for sellers, just download your bot as soon as you purchase and get ready to cop some sneakers!</Text>
@@ -73,33 +76,33 @@ export default function Explain() {
                       src={'https://media.discordapp.net/attachments/707421140179746946/858521541934317568/G4fe4a0.png'}
                     />
                   </Box>
-                </SimpleGrid>
-              </Box>
-            </Container>
-          </Center>
-        </>
 
-      :
+                    </SimpleGrid>
+            </Stack>
+          </>
 
-        <>
-          <Center mt="20px">
-            <Flex flexDirection="row">
-              <Center mr="25px">
-                <Text fontWeight="medium" onClick={() => setexplain(true)}>Buying</Text> 
-              </Center>
-              <Center ml="25px">
-                <Text color="gray.600" textDecoration="underline" textDecorationColor="#81FF62" fontWeight="medium" onClick={() => setexplain(false)}>Selling</Text> 
-              </Center>
-            </Flex>
-          </Center>
-          <Center>
-            <Text mt="3rem" fontSize={{ base: '0.8rem', sm: '', lg: "1rem" }} fontWeight="medium">Coming soon ...</Text>
-          </Center>
-        </>
+          :
+
+          <>
+            <Center mt="20px">
+              <Flex flexDirection="row">
+                <Center mr="25px">
+                  <Text fontWeight="medium" onClick={() => setexplain(true)}>Buying</Text>
+                </Center>
+                <Center ml="25px">
+                  <Text color="gray.600" textDecoration="underline" textDecorationColor="#2F80CB" fontWeight="medium" onClick={() => setexplain(false)}>Selling</Text>
+                </Center>
+              </Flex>
+            </Center>
+            <Center>
+              <Text mt="3rem" fontSize={{ base: '0.8rem', sm: '', lg: "1rem" }} fontWeight="medium">Coming soon ...</Text>
+            </Center>
+          </>
       }
+      </Stack>
+    </Stack>
     </>
   );
 }
-  
-  
-  
+
+
