077f4cba07f85eaf2bae7a186f82a36bd5fc5c75
Tixwell <teoc.cricelli@gmail.com>
2022-07-11 17:33:46 +0200

    [Fix] eth svg

--- src/components/svg/ethereum.tsx
+++ src/components/svg/ethereum.tsx
@@ -1,15 +1,15 @@
 const EthereumSvg = () => {
-    return(
+    return (
         <svg width="23" height="37" viewBox="0 0 23 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" clipRule="evenodd" d="M11.0618 13.3852L22.0642 18.3612L11.0618 24.8589L0.0389099 18.3435L11.0618 13.3852Z" fill="#D5D5D5"/>
-        <path fillRule="evenodd" clipRule="evenodd" d="M11.0618 13.4262L22.0232 18.364L11.0618 24.8179L0.100403 18.3435L11.0618 13.4262Z" fill="#454A75"/>
-        <path d="M22.1175 18.3496L11.0577 0L11.0618 24.8793L22.1175 18.3496Z" fill="#62688F" fillOpacity="0.836391"/>
-        <path d="M11.0577 0L0 18.3496L11.0618 24.8589L11.0577 0Z" fill="#8A92B2" fillOpacity="0.812612"/>
-        <path d="M11.0577 26.9815V36.0313L22.1236 20.4477L11.0577 26.9815Z" fill="#62688F" fillOpacity="0.810098"/>
-        <path d="M0 20.4477L11.0577 36.0313V26.9815L0 20.4477Z" fill="#8A92B2" fillOpacity="0.807143"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M11.0618 13.3852L22.0642 18.3612L11.0618 24.8589L0.0389099 18.3435L11.0618 13.3852Z" fill="#D5D5D5" />
+            <path fillRule="evenodd" clipRule="evenodd" d="M11.0618 13.4262L22.0232 18.364L11.0618 24.8179L0.100403 18.3435L11.0618 13.4262Z" fill="#454A75" />
+            <path d="M22.1175 18.3496L11.0577 0L11.0618 24.8793L22.1175 18.3496Z" fill="#62688F" fillOpacity="0.836391" />
+            <path d="M11.0577 0L0 18.3496L11.0618 24.8589L11.0577 0Z" fill="#8A92B2" fillOpacity="0.812612" />
+            <path d="M11.0577 26.9815V36.0313L22.1236 20.4477L11.0577 26.9815Z" fill="#62688F" fillOpacity="0.810098" />
+            <path d="M0 20.4477L11.0577 36.0313V26.9815L0 20.4477Z" fill="#8A92B2" fillOpacity="0.807143" />
         </svg>
     );
 }
-    
+
 
 export default EthereumSvg;
